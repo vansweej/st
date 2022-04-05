0016a245064c38e6b23428bf9f4041a2bf62eb8a
Jan Van Sweevelt <vansweej@gmail.com>
2022-04-06 00:20:12 +0200

    added nightowl terminal theme

--- config.def.h
+++ config.def.h
@@ -99,31 +99,33 @@ float alpha = 0.8;
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
 	/* 8 normal colors */
-	"black",
-	"red3",
-	"green3",
-	"yellow3",
-	"blue2",
-	"magenta3",
-	"cyan3",
-	"gray90",
+	"#011627",
+	"#ef5350",
+	"#22da6e",
+	"#c5e478",
+	"#82aaff",
+	"#c792ea",
+	"#21c7a8",
+	"#ffffff",
 
 	/* 8 bright colors */
-	"gray50",
-	"red",
-	"green",
-	"yellow",
-	"#5c5cff",
-	"magenta",
-	"cyan",
-	"white",
+	"#575656",
+	"#ef5350",
+	"#22da6e",
+	"#ffeb95",
+	"#82aaff",
+	"#c792ea",
+	"#7fdbca",
+	"#ffffff",
 
 	[255] = 0,
 
 	/* more colors can be added after 255 to use with DefaultXX */
-	"#cccccc",
-	"#555555",
-	"black",
+	"#d6deeb",   /* foreground */
+	"#011627",   /* background */
+	"#d6deeb",   /* cursor_bg */
+    "#011627",   /* cursor_fg */
+    "#1b90dd"    /* selection_bg */
 };
 
 
@@ -131,10 +133,10 @@ static const char *colorname[] = {
  * Default colors (colorname index)
  * foreground, background, cursor, reverse cursor
  */
-unsigned int defaultfg = 7;
-unsigned int defaultbg = 258;
-static unsigned int defaultcs = 256;
-static unsigned int defaultrcs = 257;
+unsigned int defaultfg = 256;
+unsigned int defaultbg = 257;
+static unsigned int defaultcs = 258;
+static unsigned int defaultrcs = 259;
 
 /*
  * Default shape of cursor
